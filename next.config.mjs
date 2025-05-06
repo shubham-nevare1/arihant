f0f762f318bd151aeed41a03e45e518c0ab04a62
shubham-nevare1 <shubham.nevare@schbang.com>
2025-05-07 00:00:30 +0530

    Fix next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      unoptimized: true,
-    },
-    env: {
-      BASE_API_URL: process.env.BASE_API_URL,
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    BASE_API_URL: process.env.BASE_API_URL,
+  },
+};
+
+export default nextConfig;
